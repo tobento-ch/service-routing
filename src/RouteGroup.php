edcd66b65464b76a654497f73fed708f67f1c686
TOBENTOch <info@tobento.ch>
2024-05-09 09:03:59 +0200

    resource as object

--- src/RouteGroup.php
+++ src/RouteGroup.php
@@ -218,11 +218,11 @@ class RouteGroup implements RouteGroupInterface
      * Create a new RouteResource.
      * 
      * @param string $name The resource name
-     * @param string $controller The controller
+     * @param string|object $controller The controller
      * @param string $placeholder The placeholder name for the uri
      * @return RouteResourceInterface
      */
-    public function resource(string $name, string $controller, string $placeholder = 'id'): RouteResourceInterface
+    public function resource(string $name, string|object $controller, string $placeholder = 'id'): RouteResourceInterface
     {
         $resource = $this->router->getRouteFactory()->createRouteResource(
             $this->router,
